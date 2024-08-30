98096528c81686b5d66aedf3d4e604ba0dc0ce0a
zxmiaosenlin <3635298993@qq.com>
2024-08-30 10:03:02 +0800

    home-banner轮播图

--- src/apis/layout.js
+++ src/apis/layout.js
@@ -1,6 +1,6 @@
 import httpInstance from "@/utils/http"
 export function getCategoryAPI() {
   return httpInstance({
-    url: 'https://pcapi-xiaotuxian-front-devtest.itheima.net/home/category/head'
+    url: '/home/category/head'
   })
 }
--- src/utils/http.js
+++ src/utils/http.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const httpInstance = axios.create({
-  baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
+  baseURL: 'https://pcapi-xiaotuxian-front-devtest.itheima.net',
   timeout: 5000
 })
 

073322ad3a34c694bbe9acae44d214a1301c3bad
zxmiaosenlin <3635298993@qq.com>
2024-08-31 09:17:46 +0800

    列表筛选功能实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -24,11 +24,15 @@ const reqData = ref({
 })
 const getGoodList = async () => {
   const res = await getSubCategoryAPI(reqData.value)
-  console.log(res);
-  
   goodList.value = res.data.result.items
 }
 onMounted(() => { getGoodList() })
+
+//tab切换回调
+const tabChange = () => {
+  reqData.value.page = 1
+  getGoodList()
+}
 </script>
 
 <template>
@@ -43,7 +47,7 @@ onMounted(() => { getGoodList() })
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>

5bc2c5acb11f8242c939ae488bf0363af7c26d75
zxmiaosenlin <3635298993@qq.com>
2024-08-31 09:37:49 +0800

    二级分类-列表无限加载实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -33,6 +33,16 @@ const tabChange = () => {
   reqData.value.page = 1
   getGoodList()
 }
+
+//加载更多
+const disabled = ref(false)
+const load = async () => {
+  //获取下一页数据
+  reqData.value.page += 1
+  const res = await getSubCategoryAPI(reqData.value)
+  goodList.value = [...goodList.value, ...res.data.result.items]
+  if(res.data.result.items.length == 0) disabled.value = true
+}
 </script>
 
 <template>
@@ -52,7 +62,7 @@ const tabChange = () => {
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
         <!-- 商品列表-->
       <GoodsItem v-for="goods in goodList" :key="goods.id" :goods="goods"/>
       </div>

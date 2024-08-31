6f64b6d2c8208e92994bcd4431f0b06531e80cd1
zxmiaosenlin <3635298993@qq.com>
2024-08-31 14:14:59 +0800

    详情基础数据渲染

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -1,20 +1,32 @@
 <script setup>
+import { getDetailAPI } from '@/apis/detail'
+import { onMounted } from 'vue';
+import { ref } from 'vue';
+import { useRoute } from 'vue-router';
 
+const detailList = ref({})
+const route = useRoute()
+const detailAPI = async () => {
+  const res = await getDetailAPI(route.params.id)
+  console.log(res)
+  detailList.value = res.data.result
+}
+onMounted(() => { detailAPI() })
 
 </script>
 
 <template>
   <div class="xtx-goods-page">
-    <div class="container">
+    <div class="container" v-if="detailList.details">
       <!-- 面包屑导航区域 -->
-      <div class="bread-container">
+      <div class="bread-container" >
         <el-breadcrumb separator=">">
           <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-          <el-breadcrumb-item :to="{ path: '/' }">母婴
-          </el-breadcrumb-item>
-          <el-breadcrumb-item :to="{ path: '/' }">跑步鞋
-          </el-breadcrumb-item>
-          <el-breadcrumb-item>抓绒保暖，毛毛虫子儿童运动鞋</el-breadcrumb-item>
+          <el-breadcrumb-item :to="{ path: `/category/${detailList.categories[1].id}` }">{{
+            detailList.categories[1].name }}</el-breadcrumb-item>
+          <el-breadcrumb-item :to="{ path: `/category/sub/${detailList.categories[0].id}` }">{{
+            detailList.categories[0].name }}</el-breadcrumb-item>
+          <el-breadcrumb-item>{{ detailList.desc }}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
       <!-- 商品信息 -->
@@ -28,33 +40,33 @@
               <ul class="goods-sales">
                 <li>
                   <p>销量人气</p>
-                  <p> 100+ </p>
+                  <p> {{ detailList.salesCount }}+ </p>
                   <p><i class="iconfont icon-task-filling"></i>销量人气</p>
                 </li>
                 <li>
                   <p>商品评价</p>
-                  <p>200+</p>
+                  <p>{{ detailList.commentCount }}+</p>
                   <p><i class="iconfont icon-comment-filling"></i>查看评价</p>
                 </li>
                 <li>
                   <p>收藏人气</p>
-                  <p>300+</p>
+                  <p>{{ detailList.collectCount }}+</p>
                   <p><i class="iconfont icon-favorite-filling"></i>收藏商品</p>
                 </li>
                 <li>
                   <p>品牌信息</p>
-                  <p>400+</p>
+                  <p>{{ detailList.brand.name }}</p>
                   <p><i class="iconfont icon-dynamic-filling"></i>品牌主页</p>
                 </li>
               </ul>
             </div>
             <div class="spec">
               <!-- 商品信息区 -->
-              <p class="g-name"> 抓绒保暖，毛毛虫儿童鞋 </p>
-              <p class="g-desc">好穿 </p>
+              <p class="g-name"> {{detailList.name}} </p>
+              <p class="g-desc">{{ detailList.desc }} </p>
               <p class="g-price">
-                <span>200</span>
-                <span> 100</span>
+                <span>{{ detailList.price }}</span>
+                <span>{{ detailList.oldPrice }}</span>
               </p>
               <div class="g-service">
                 <dl>
@@ -94,13 +106,14 @@
                 <div class="goods-detail">
                   <!-- 属性 -->
                   <ul class="attrs">
-                    <li v-for="item in 3" :key="item.value">
-                      <span class="dt">白色</span>
-                      <span class="dd">纯棉</span>
+                    <li v-for="item in detailList.details.properties" :key="item.value">
+                      <span class="dt">{{item.name}}</span>
+                      <span class="dd">{{item.value}}</span>
                     </li>
                   </ul>
                   <!-- 图片 -->
-
+                   <!-- 这里我用了懒加载 -->
+                  <img v-for="i in detailList.details.pictures" v-img-lazy="i" :key="i.value"  alt="">
                 </div>
               </div>
             </div>

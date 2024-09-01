2872c10b517ca7c066b246aa5f5a1056b366f349
zxmiaosenlin <3635298993@qq.com>
2024-09-01 15:25:33 +0800

    登录-请求拦截器携带token

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,7 @@
 import axios from 'axios'
 import 'element-plus/theme-chalk/el-message.css'
 import { ElMessage } from 'element-plus'
+import { useUserStore } from '@/stores/user';
 
 const httpInstance = axios.create({
   baseURL: 'https://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -10,31 +11,26 @@ const httpInstance = axios.create({
 
 //拦截器
 
-//axios请求拦截器
-/* httpInstance.interceptors.request.use(config => {
-  return config
-}), e=> Promise.reject((e))
-
-//axios响应拦截器
-httpInstance.interceptors.response.use(res => {
-  return res.data, e => Promise.reject((e))
-})
- */
 // 添加请求拦截器
-httpInstance.interceptors.request.use(function (config) {
+httpInstance.interceptors.request.use( config => {
+  const useStore = useUserStore()
+  const token = useStore.userInfo.token
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
   // 在发送请求之前做些什么
   return config;
-}, function (error) {
+}, error => {
   // 对请求错误做些什么
   return Promise.reject(error);
 });
 
 // 添加响应拦截器
-httpInstance.interceptors.response.use(function (response) {
+httpInstance.interceptors.response.use(response => {
   // 2xx 范围内的状态码都会触发该函数。
   // 对响应数据做点什么
   return response;
-}, function (error) {
+}, error => {
   // 超出 2xx 范围的状态码都会触发该函数。
   // 对响应错误做点什么
   // 统一错误提示

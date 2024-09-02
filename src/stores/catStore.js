8de9e56fdfc4faed2e05fafc3b4dab31f6225d09
zxmiaosenlin <3635298993@qq.com>
2024-09-02 10:23:47 +0800

    接口购物车-加入购物车实现

--- src/stores/catStore.js
+++ src/stores/catStore.js
@@ -1,29 +1,44 @@
 //封装购物车模块
 import { defineStore } from "pinia"
 import { computed, ref } from "vue"
+import { useUserStore } from "./user"
+import { insertCartAPI } from '@/apis/cart'
+import { findNewCartListAPI } from '@/apis/cart'
 
 export const useCartStore = defineStore('cat', () => {
+  const userStore = useUserStore()
+  const isLogin = computed(() => userStore.userInfo.token)
   //state-cartList(购物车列表)
   const cartList = ref([])
   //action-添加购物车方法
-  const addCard = (goods) => {
-    //已添加过-count + 1
-    //没添加过-直接push
-    //思路：已经传过来的数据，如果skuId在已有数据中存在，则加1，如果不存在则直接push
-    //array.find返回满足条件的第一个单元值
-    //这里是输入的id和购物车已有的id匹配，有的话返回满足条件的第一个（这里应该也是只有一个）对象
-    const item = cartList.value.find((item) => goods.skuId === item.skuId)
-    if (item) {
-      //返回的对象里面的count进行变更
-      item.count += goods.count
-      console.log(cartList.value);
-
+  const addCard = async (goods) => {
+    const { skuId, count } = goods
+    if (isLogin.value) {
+      //登录之后的加入购物车逻辑
+      //调用加入购物车接口
+      await insertCartAPI({ skuId, count })
+      //调用获取购物车列表接口
+      const res = await findNewCartListAPI()
+      //用接口购物车列表覆盖本地购物车列表
+      cartList.value = res.data.result
     } else {
-      //如果不存在一样的id 那直接把新获得的对象加入数组中
-      cartList.value.push(goods)
-      console.log(cartList.value);
+      //已添加过-count + 1
+      //没添加过-直接push
+      //思路：已经传过来的数据，如果skuId在已有数据中存在，则加1，如果不存在则直接push
+      //array.find返回满足条件的第一个单元值
+      //这里是输入的id和购物车已有的id匹配，有的话返回满足条件的第一个（这里应该也是只有一个）对象
+      const item = cartList.value.find((item) => goods.skuId === item.skuId)
+      if (item) {
+        //返回的对象里面的count进行变更
+        item.count += goods.count
+      } else {
+        //如果不存在一样的id 那直接把新获得的对象加入数组中
+        cartList.value.push(goods)
+      }
     }
   }
+
+  //删除购物车
   const delCart = (skuId) => {
     //思路：数组中删除某一项
     //方法一：找到要删除项的下标 使用splice

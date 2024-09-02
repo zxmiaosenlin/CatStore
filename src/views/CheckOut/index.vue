97a8a8ba60e6acb5f769af90e9b2282ade5cb663
zxmiaosenlin <3635298993@qq.com>
2024-09-02 14:45:07 +0800

    切换地址-激活交互实现

--- src/views/CheckOut/index.vue
+++ src/views/CheckOut/index.vue
@@ -2,21 +2,31 @@
 import { getCheckInfoAPI } from '@/apis/checkout'
 import { onMounted } from 'vue';
 import { ref } from 'vue';
+
 const checkInfo = ref({})  // 订单对象
-const defaultAddress = ref({})
+const defaultAddress = ref({}) //默认地址对象
+
+//调用接口函数 获取订单信息
 const getCheckInfo = async () => {
   const res = await getCheckInfoAPI()
-  console.log(res.data.result);
-  
   checkInfo.value = res.data.result
   //适配默认地址
   defaultAddress.value = checkInfo.value.userAddresses.find(item => item.isDefault === 0)
-  console.log(defaultAddress.value)
-  
 }
 onMounted(() => { getCheckInfo() })
 //控制弹框打开
 const showDialog = ref(false)
+//切换地址
+const activeAddress = ref({})
+const switchAddress = (item) => {
+  activeAddress.value = item
+}
+//确认切换默认地址
+const confirmAddress = () => {
+  defaultAddress.value = activeAddress.value
+  //关闭弹窗
+  showDialog.value = false
+}
 </script>
 
 <template>
@@ -119,7 +129,7 @@ const showDialog = ref(false)
   <!-- 切换地址 -->
   <el-dialog v-model="showDialog" title="切换收货地址" width="30%" center>
     <div class="addressWrapper">
-      <div class="text item" v-for="item in checkInfo.userAddresses" :key="item.id">
+      <div class="text item" @click="switchAddress(item)" v-for="item in checkInfo.userAddresses" :key="item.id" :class="{ active: item.id === activeAddress.id }">
         <ul>
           <li><span>收<i />货<i />人：</span>{{ item.receiver }} </li>
           <li><span>联系方式：</span>{{ item.contact }}</li>
@@ -130,7 +140,7 @@ const showDialog = ref(false)
     <template #footer>
       <span class="dialog-footer">
         <el-button>取消</el-button>
-        <el-button type="primary">确定</el-button>
+        <el-button type="primary" @click="confirmAddress">确定</el-button>
       </span>
     </template>
   </el-dialog>

3395d2472a964f3a810ef3e92a95eb73cd7e6655
zxmiaosenlin <3635298993@qq.com>
2024-09-03 08:35:08 +0800

    会员中心-个人信息数据渲染

--- src/views/Member/components/UserInfo.vue
+++ src/views/Member/components/UserInfo.vue
@@ -1,5 +1,19 @@
 <script setup>
-const userStore = {}
+import { useUserStore } from '@/stores/user'
+import { getLikeListAPI } from '@/apis/user'
+import GoodsItem from '@/views/Home/components/GoodsItem.vue'
+import { ref } from 'vue';
+import { onMounted } from 'vue';
+
+const userStore = useUserStore()
+const likeList = ref({})
+const getLikeList = async () => {
+  const res = await getLikeListAPI({ limit: 4 })
+  likeList.value = res.data.result
+}
+
+onMounted(() => { getLikeList() })
+
 </script>
 
 <template>
@@ -32,7 +46,7 @@ const userStore = {}
         <h4 data-v-bcb266e0="">猜你喜欢</h4>
       </div>
       <div class="goods-list">
-        <!-- <GoodsItem v-for="good in likeList" :key="good.id" :good="good" /> -->
+        <GoodsItem v-for="good in likeList" :key="good.id" :goods="good" />
       </div>
     </div>
   </div>
